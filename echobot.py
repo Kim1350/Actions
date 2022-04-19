e15947b5038b72355c648d2cd485de840b8b60aa
Kim1350 <rholmogorov12@gmail.com>
2022-04-19 12:33:20 +0900

    fix2

--- echobot.py
+++ echobot.py
@@ -50,7 +50,7 @@ def echo(update: Update, context: CallbackContext) -> None:
 def main() -> None:
     """Start the bot."""
     # Create the Updater and pass it your bot's token.
-    updater = Updater("TOKEN")
+    updater = Updater("5260272231:AAFhEYhkDVN-v7-jTAcyXa0kMykxO4P_EKA")
 
     # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
